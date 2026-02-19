bc98f79b0c5580bdb5b4098e70b86979327570d4
SangaviMathi <113693837+SangaviMathi@users.noreply.github.com>
2026-02-19 13:37:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool
+import os
+load_dotenv()
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+print("API KEY LOADED:", bool(os.getenv("GOOGLE_API_KEY")))
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human","{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+
+).partial(format_instructions=parser.get_format_instructions())
+tools = [search_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("what can i help you?")
+raw_response = agent_executor.invoke({"query": query})
+print(raw_response)
+
+
+
+
+
+
+
+
+
+
+
